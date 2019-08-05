4be4c46c2302cdd29c7a51b3da2471b32263ae1b
V.Adam <V.Adam@cip-ws-128.cip.physik.uni-muenchen.de>
2019-08-05 10:04:06 +0200

    added the swapping function

--- swap.c
+++ swap.c
@@ -9,6 +9,9 @@ int main(void)
     printf("y is %i\n", y);
     
     // swapping...
+    int bucket = x;
+    x = y;
+    y = bucket;
     
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
